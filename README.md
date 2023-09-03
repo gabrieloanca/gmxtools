5031cc00166ab4d51e94bd949695fec0795bfdc5
Gabriel Oanca <oanca.gabriel@gmail.com>
2023-09-03 14:23:33 +0200

    Sometimes QFEP shifts the PS minimum at 0 instead of RS, on '<dGg norm>' column, and qstats.py have been modified to go around that. Also minor modifications to the README file.

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-gmxtools is a series of python scripts for running EVB simulations in gromacs.
+gmxtools are a series of python scripts for EVB simulations with gromacs.
 
 For download and updates, vizit or clone:
-    https://github.com/gabrieloanca/gmxtools.git
+    https://github.com/gabrieloanca/gmxtools.git or
     git@github.com:gabrieloanca/gmxtools.git
 For suggestions, reporting bugs or for any assistance write to oanca.gabriel@gmail.com
 
-For easier use, ddd the path to this folder into your bash rc file.
+For easier use, add this folder to the PATH environment variable in the bash rc file
 
 
 The following scripts are available:
-   ffld2gmx.py     - converts ffld parameters to OPLS-AA types fro Gromacs
+   ffld2gmx.py     - converts ffld parameters to OPLS-AA types for Gromacs
    genposre.py     - generates posre files with different constraints for region 1 and region 2
-   gmx2qfep.py     - writes outputs from 'gmx energy' into qfep5_gmx format
+   gmx2qfep.py     - writes the output from 'gmx energy' into qfep5_gmx format
    gmx4evb.py      - builds topologies, one for each FEP frame
    qfep5_gmx       - Q5 mapping to extract the EVB profile (fortran code)
-   qstats.py       - extracts the mean and the standard deviation from qfep5_gmx output files
-   poly_data.py    - smoothens qfep5_gmx EVB generated data by a 6th degree polynomial function
+   qstats.py       - calculates the mean and the standard deviation from qfep5_gmx output files
+   poly_data.py    - smoothens qfep5_gmx EVB generated data with a 6th degree polynomial function
 
 
 To show this list in your terminal, type *gmxtools*
--- qstats_lambda.py
+++ qstats_lambda.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-# #### Date: June 27 2023
-# #### This script extrats the EVB profiles from qfep output files (Part 3) and calculates the mean and std for dG# and dG0
+## Sept 1 2023
 
-# beer-ware licence
-# oanca.gabriel@gmail.com
+## This script reads QFEP output files and return dG# and dG0 plus STD from lambda profiles (under 'Part 1')
 
-# For download and updates, vizit or clone:
-#     https://github.com/gabrieloanca/gmxtools.git
-#     git@github.com:gabrieloanca/gmxtools.git
-# For suggestions, reporting bugs or for any assistance write to oanca.gabriel@gmail.com
-
+## Usage: ./qstats_lambda.py <input file>
 
 import sys, os, re
 import statistics as st
@@ -35,7 +29,7 @@ def names(fname):
 
     base = ''.join(pre)
     return base
-    
+
 def get_files(base):
     files = os.listdir()
     fdata=[]
@@ -43,10 +37,12 @@ def get_files(base):
         if re.search(base, f):
             if (f[0] == '#'):
                 continue
-            elif 'graph' in f[f.index('.'):]:
+            elif ('graph' in f[f.index('.'):]) or ('lambda' in f[f.index('.'):]):
                 continue
             else:
                 fdata.append(f)
+
+    fdata.sort()
     return fdata
 
 def get_profiles(fdata):
@@ -57,56 +53,54 @@ def get_profiles(fdata):
         with open(profile) as f:
             data = f.read().strip().split("\n")
         
-        for i, line in enumerate(data):
-            if "# Part 3:" in line:
-                click = i
+        for i,l in enumerate(data):
+            if "Part 1:" in l:
+                start1 = i+4
+            elif "Part 2:" in l:
+                start2 = i+2
+                stop1 = i-7
 
-        for line in data[click+2:]:
+        for line in data[start1:stop1+1]:
             l = line.strip().split()
-            profiles[profile]['x'].append(float(l[1]))
-            profiles[profile]['y'].append(float(l[3]))
+            profiles[profile]['x'].append(float(l[0]))
+            profiles[profile]['y'].append(float(l[5]))
     
         del(data)
     return profiles
 
 def stats(profiles):
     ts_data, ps_data = [], []
-    rs_list = {}  # I save these to shift the profiles since <dGg norm> in QFEP gets shifted at zero at the lowest state, which is not always RS
     keys = list(profiles.keys())
-    ts_loc = int(len(profiles[keys[0]]['x'])/2)
     for key in keys:
-        rs = min(profiles[key]['y'][:ts_loc])
-        rs_loc = profiles[key]['y'][:ts_loc].index(rs)
-        ps = min(profiles[key]['y'][ts_loc:])
-        ps_loc = profiles[key]['y'][ts_loc:].index(ps) + ts_loc
-        ts = max(profiles[key]['y'][rs_loc:ps_loc])
+        ps = profiles[key]['y'][-1]
+        ts = max(profiles[key]['y'])
 
-        rs_list[key] = rs
-        ps_data.append(ps-rs)
-        ts_data.append(ts-rs)
+        ps_data.append(ps)
+        ts_data.append(ts)
 
     ts_mean = st.mean(ts_data)
     ts_std = st.stdev(ts_data)
     ps_mean = st.mean(ps_data)
     ps_std = st.stdev(ps_data)
 
-    return ts_mean, ts_std, ps_mean, ps_std, rs_list
+    return ts_mean, ts_std, ps_mean, ps_std
 
-def savefig(profiles, rs_list, name='evb_profile'):
+def savefig(profiles, name='evb_profile'):
     keys = list(profiles.keys())
     fig = plt.figure()
     fig.set_figheight(3)
     fig.set_figwidth(4)
     ax1 = fig.add_axes([0, 0, 1, 1])
     ax1.grid()
-    ax1.set_xlabel(r'$\epsilon_1$ - $\epsilon_2$ (kcal/mol)')
+    ax1.set_xlabel(r'$\lambda$')
     ax1.set_ylabel(r'$\Delta$G (kcal/mol)')
     
     for key in keys:
-        ax1.plot(profiles[key]['x'], [i - rs_list[key] for i in profiles[key]['y']])
+        ax1.plot(profiles[key]['x'], profiles[key]['y'])
+    ax1.invert_xaxis()
     fig.savefig(name,dpi = 300,orientation='landscape',facecolor='w',edgecolor='w',bbox_inches='tight')
 
-def savedata(profiles, rs_list):
+def savedata(profiles):
     keys = list(profiles.keys())
     keys.sort()
     ts_loc = int(len(profiles[keys[0]]['x'])/2)
@@ -117,9 +111,9 @@ def savedata(profiles, rs_list):
             pre = key[:key.index('.')]
         except:
             pre = key
-        file = open(f"{pre}.graph", "w")
+        file = open(f"{pre}.lambda", "w")
         for i in range(len(profiles[key]['y'])):
-            file.write(f"{profiles[key]['x'][i]}\t{profiles[key]['y'][i] - rs_list[key]:.2f}\n")
+            file.write(f"{profiles[key]['x'][i]: >7.3f}\t{profiles[key]['y'][i]: >8.3f}\n")
         file.close()
 
 if __name__ == "__main__":
@@ -127,7 +121,7 @@ if __name__ == "__main__":
         fname = list(sys.argv[1])
     except:
         print()
-        print('Usage: get_profiles.sh <data_file>')
+        print('Usage: qstats_lambda.py <data_file>')
         print('where "data_file" is any of the EVB data files.')
         print("Requirement: the names of the data files be numbered and must start with an alphabetic character. E.g.: rep0.dat, rep1.dat, etc.")
         print()
@@ -150,18 +144,15 @@ if __name__ == "__main__":
         sys.exit()
 
     try:
-        tmean, tstd, pmean, pstd, rs_list = stats(profiles)
+        tmean, tstd, pmean, pstd = stats(profiles)
 
         print(f'\ndG#: {tmean:5.2f}  +/-{tstd:5.2f}')
         print(f'dG0: {pmean:5.2f}  +/-{pstd:5.2f}')
     except:
-        print("\nEVB profiles could not be analyzed.")
+        print("\nlambda profiles could not be analyzed.")
         sys.exit()
 
-    name='evb_profile.png'
-    savefig(profiles, rs_list, name)
-    savedata(profiles, rs_list)
+    name='lambda_profiles.png'
+    savefig(profiles, name)
+    savedata(profiles)
     print(f'For a graphical representation, see "{name}".\n')
-
-
-
